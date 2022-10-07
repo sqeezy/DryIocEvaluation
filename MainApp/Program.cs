547c4adb222f9ca1ef0bf54530da74fb0e23b415
Anton Herzog <info@antonherzog.com>
2022-10-07 08:55:13 +0200

    WIP more tests

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -3,28 +3,43 @@ using Dep1;
 using DryIoc;
 using DryIoc.MefAttributedModel;
 using DryIocAttributes;
+using static System.Console;
 
-// var container = new Container();
-var container = new Container().WithMefAttributedModel(); 
+void ServiceHierarchyTest(Container c)
+{
+    // var service = container.Resolve<MyService>();
+    // var serviceAsInterface = container.Resolve<IMyService>();
+    // var serviceAsInterfaceToTheSide = container.Resolve<IServiceToTheSideOfMyService>();
+    var serviceAsMiddleInterface = c.Resolve<IMostSpecificInterface>();
+    var serviceAsMiddleInterface2 = c.Resolve<IMostSpecificInterface>();
+
+    c.Dispose();
+
+    WriteLine($"{serviceAsMiddleInterface2 == serviceAsMiddleInterface}");
+    WriteLine($"{serviceAsMiddleInterface.IsDisposed}");
+    WriteLine($"{serviceAsMiddleInterface2.IsDisposed}");
+}
+
+var container = new Container(rules => rules.WithDefaultReuse(Reuse.Singleton));
+// var container = new Container().WithMefAttributedModel(); 
 
 // container.RegisterMany<MyService>(Reuse.Singleton, serviceTypeCondition: type => type.IsInterface);
 
 container.RegisterExports(typeof(MyService).GetAssembly());
 
-// var service = container.Resolve<MyService>();
-// var serviceAsInterface = container.Resolve<IMyService>();
-// var serviceAsInterfaceToTheSide = container.Resolve<IServiceToTheSideOfMyService>();
-var serviceAsMiddleInterface = container.Resolve<IMostSpecificInterface>();
-var serviceAsMiddleInterface2 = container.Resolve<IMostSpecificInterface>();
+//ServiceHierarchyTest(container);
 
-container.Dispose();
+var thing = container.Resolve<IOperation<bool, bool>>();
+var thing2 = container.Resolve<IOperation<object, string>>();
+WriteLine(thing.GetType());
+WriteLine(thing2.GetType());
+public interface IOperation<TSubject, TScope>{}
 
-Console.WriteLine($"{serviceAsMiddleInterface2 == serviceAsMiddleInterface}");
-Console.WriteLine($"{serviceAsMiddleInterface.IsDisposed}");
-Console.WriteLine($"{serviceAsMiddleInterface2.IsDisposed}");
+[ExportMany]
+public class OperationClosed : IOperation<bool, bool>{}
 
-
-
+[ExportMany]
+public class OperationOpen<TSubject, TScope> : IOperation<TSubject, TScope>{}
 
 public interface IServiceToTheSideOfMyService
 {
